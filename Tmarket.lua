f4587f1d35f6828e33296790f5329d733d8fd22e
legacу <harveycompanystart@gmail.com>
2025-05-18 14:25:00 +0300

    Update Tmarket.lua

--- Tmarket.lua
+++ Tmarket.lua
@@ -1,6 +1,6 @@
 script_name("Tmarket")
 script_author("legacy.")
-script_version("1.71")
+script_version("1.72")
 
 local ffi = require("ffi")
 local encoding = require("encoding")
@@ -223,7 +223,7 @@ function main()
     if checkNick(cachedNick) then
         downloadConfigFile(function()
             loadData()
-            sampAddChatMessage("{4169E1}[Tmarket]{FFFFFF} Активация: {1E90FF}/tmarket{FFFFFF}. Автор – {1E90FF}legacy{FFFFFF}.", -1)
+sampAddChatMessage(string.format("{4169E1}[Tmarket v%s]{FFFFFF} Активация: {1E90FF}/tmarket{FFFFFF}. Автор – {1E90FF}legacy{FFFFFF}.", thisScript().version), -1)
             sampRegisterChatCommand("tmarket", function() window[0] = not window[0] end)
         end)
     else
